4f950cf64abb7108aaaf7980a17ec8e73a49d15d
borsoe <borsoe.reza@gmail.com>
2026-01-03 17:55:28 +0330

    test: explicit user definition

--- hosts/asusLaptop/default.nix
+++ hosts/asusLaptop/default.nix
@@ -1,14 +1,26 @@
-{ config, ... }:
+# { config, ... }:
 
+# {
+#   imports = [
+#     ./configuration.nix
+#     ./hardware-configuration.nix
+#   ];
+
+#   config = {
+#     home-manager.users = builtins.listToAttrs
+#       (map (user: { name = user; value =
+#                       { imports = [ ./home.nix ../../modules/user ]; };}) config.systemSettings.users);
+#     };
+# }
+{ config, ... }:
 {
   imports = [
     ./configuration.nix
     ./hardware-configuration.nix
   ];
-
   config = {
-    home-manager.users = builtins.listToAttrs
-      (map (user: { name = user; value =
-                      { imports = [ ./home.nix ../../modules/user ]; };}) config.systemSettings.users);
+    home-manager.users.borsoe = {
+      imports = [ ./home.nix ../../modules/user ];
     };
+  };
 }
