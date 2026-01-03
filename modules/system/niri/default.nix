34e3b4b7bc636adea2338f530e9d085a224dead2
borsoe <borsoe.reza@gmail.com>
2026-01-03 22:19:09 +0330

    rice: sddm

--- modules/system/niri/default.nix
+++ modules/system/niri/default.nix
@@ -21,6 +21,8 @@ in
     nixpkgs.overlays = [ inputs.niri.overlays.niri ];
     programs.niri.package = pkgs.niri-unstable;
     environment.variables.NIXOS_OZONE_WL = "1";
+
+    # Necessary packages
     environment.systemPackages = with pkgs; [
       wl-clipboard
       wayland-utils
@@ -28,8 +30,30 @@ in
       cage
       gamescope
       xwayland-satellite-unstable
+      (sddm-astronaut.override {
+        themeConfig = {
+          # TODO Update Theme Config
+          # https://github.com/Keyitdev/sddm-astronaut-theme/blob/master/Themes/astronaut.conf
+          background = config.stylix.image;
+          ScreenWidth = 1920;
+          ScreenHeight = 1080;
+          blur = false;
+        };
+      })
     ];
-    services.displayManager.sddm.enable = true;
-    services.displayManager.sddm.wayland.enable = true;
+
+    # Display manager
+    services.xserver.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      enableHidpi = true;
+      theme = "sddm-astronaut-theme";
+      package = pkgs.kdePackages.sddm;
+      extraPackages = with pkgs; [
+        (sddm-astronaut.override {
+          embeddedTheme = "Japanese aesthetic";
+        })
+      ];
+    };
   };
 }
